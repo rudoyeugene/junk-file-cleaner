ad3629c0c59ee977efdc78f964db5ff9b0e67c95
Ievgen 'Eugene' Rudyi <rudoyeugene@gmail.com>
2025-08-09 12:28:08 +0300

    add date timestamp to track the activations

--- cleaner.sh
+++ cleaner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 while true; do
+  echo "================================"
+  echo date
   # Check if the LOCATIONS variable is set
   if [ -z "$LOCATIONS" ]; then
       echo "Error: LOCATIONS environment variable is not set. Nothing will be cleaned."
